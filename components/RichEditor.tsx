de63ea5c66a14f6088c30f5cb335923d3684be14
Cavid <cavidoguz123@gmail.com>
2026-04-18 15:44:51 +0400

    fix editor build error

--- components/RichEditor.tsx
+++ components/RichEditor.tsx
@@ -342,7 +342,7 @@ export default function RichEditor({
       lastValue.current = value;
       const current = editor.getHTML();
       if (value !== current) {
-        editor.commands.setContent(value || "", false);
+        editor.commands.setContent(value || "");
       }
     }
   }, [value, editor]);
